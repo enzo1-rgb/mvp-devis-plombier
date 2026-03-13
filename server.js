c682d3273769c929dfaf8899301210456202eceb
snowb <enzo.keti1@gmail.com>
2026-03-13 18:21:48 +0100

    fix railway port

--- server.js
+++ server.js
@@ -73,6 +73,10 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
   console.log('Server running on http://localhost:3001');
 });
