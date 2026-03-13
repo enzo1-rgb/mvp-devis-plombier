8474cd05ca264e657d801f9e5dd4fa4692442844
snowb <enzo.keti1@gmail.com>
2026-03-13 12:05:26 +0100

    Fix CORS serveur email Railway

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'http://127.0.0.1:3000',
+  'http://127.0.0.1:5173',
+  'https://mvp-devis-plombier.vercel.app',
+];
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 app.use(express.json());
 
 app.post('/send-email', async (req, res) => {
